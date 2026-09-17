519c34ae8f82c661d1060d26d609f5a54aa39217
LIghtJUNction <lightjunction.me@gmail.com>
2026-09-18 01:12:04 +0800

    test(tools): co-locate shell signal regression

--- crates/cortexfs-tools/tests/shell.rs
+++
@@ -1,6 +0,0 @@
-#[test]
-fn shell_exec_cli_reports_signal_exit_code() {
-    let args = [std::ffi::OsString::from("kill -TERM $$")];
-    let code = cortexfs_tools::run_shell_exec_cli(&args, &mut Vec::new());
-    assert!(matches!(code, Ok(code) if code == std::process::ExitCode::from(143)));
-}
